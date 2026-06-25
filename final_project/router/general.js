76a8a8520bb9381b9352a14b058bf6df2c02e206
shotwn <2876361+shotwn@users.noreply.github.com>
2026-06-26 02:22:06 +0300

    Refactor book retrieval logic and update API response handling

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,21 +5,67 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-const resolveWithAxios = async (data) => {
-  const response = await axios.get('http://localhost/virtual', {
-    adapter: async (config) => ({
+const bookClient = axios.create({
+  adapter: async (config) => {
+    const requestUrl = new URL(config.url, 'http://bookshop.local');
+    const pathParts = requestUrl.pathname.split('/').filter(Boolean);
+    let data;
+    let status = 200;
+
+    if (pathParts.length === 0) {
+      data = books;
+    } else if (pathParts[0] === 'isbn') {
+      data = books[pathParts[1]];
+    } else if (pathParts[0] === 'author') {
+      const author = decodeURIComponent(pathParts.slice(1).join('/'));
+      data = Object.values(books).filter((book) => book.author === author);
+    } else if (pathParts[0] === 'title') {
+      const title = decodeURIComponent(pathParts.slice(1).join('/'));
+      data = Object.values(books).filter((book) => book.title === title);
+    } else {
+      status = 404;
+      data = {message: "Route not found"};
+    }
+
+    return {
       data,
-      status: 200,
-      statusText: 'OK',
+      status,
+      statusText: status === 200 ? 'OK' : 'Not Found',
       headers: {},
       config,
       request: {}
-    })
-  });
+    };
+  }
+});
 
+const getAllBooks = async () => {
+  const response = await bookClient.get('/');
   return response.data;
 };
 
+const getBookByISBN = (isbn) => {
+  return new Promise((resolve, reject) => {
+    bookClient.get(`/isbn/${encodeURIComponent(isbn)}`)
+      .then((response) => {
+        if (response.data) {
+          resolve(response.data);
+        } else {
+          reject(new Error("Book not found"));
+        }
+      })
+      .catch(reject);
+  });
+};
+
+const getBooksByAuthor = async (author) => {
+  const response = await bookClient.get(`/author/${encodeURIComponent(author)}`);
+  return response.data;
+};
+
+const getBooksByTitle = async (title) => {
+  const response = await bookClient.get(`/title/${encodeURIComponent(title)}`);
+  return response.data;
+};
 
 public_users.post("/register", (req,res) => {
   const username = req.body.username;
@@ -39,22 +85,20 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/', async function (req, res) {
-  return res.json(await resolveWithAxios({...books}));
+  const bookList = await getAllBooks();
+  return res.json(bookList);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', async function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
   if (!isbn) {
     return res.status(400).json({message: "ISBN parameter is required"});
   }
 
-  const book = await resolveWithAxios(books[isbn]);
-  if (book) {
-    return res.json(book);
-  } else {
-    return res.status(404).json({message: "Book not found"});
-  }
+  getBookByISBN(isbn)
+    .then((book) => res.json(book))
+    .catch(() => res.status(404).json({message: "Book not found"}));
  });
   
 // Get book details based on author
@@ -64,7 +108,7 @@ public_users.get('/author/:author', async function (req, res) {
     return res.status(400).json({message: "Author parameter is required"});
   }
 
-  const filteredBooks = await resolveWithAxios(Object.values(books).filter(book => book.author === author));
+  const filteredBooks = await getBooksByAuthor(author);
   if (filteredBooks.length > 0) {
     return res.json(filteredBooks);
   } else {
@@ -79,7 +123,7 @@ public_users.get('/title/:title', async function (req, res) {
     return res.status(400).json({message: "Title parameter is required"});
   }
 
-  const filteredBooks = await resolveWithAxios(Object.values(books).filter(book => book.title === title));
+  const filteredBooks = await getBooksByTitle(title);
   if (filteredBooks.length > 0) {
     return res.json(filteredBooks);
   } else {
@@ -94,7 +138,7 @@ public_users.get('/review/:isbn', async function (req, res) {
     return res.status(400).json({message: "ISBN parameter is required"});
   }
 
-  const book = await resolveWithAxios(books[isbn]);
+  const book = books[isbn];
   if (book) {
     return res.json(book.reviews);
   } else {
